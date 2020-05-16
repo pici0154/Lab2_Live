155a12be31acf3738d904836255487ce327d0214
Ionut <ionutpop26@yahoo.com>
2020-05-16 19:54:10 +0300

    Add project files.

---
+++ Lab2_Live/Models/CostItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2_Live.Models
+{
+    public enum CostType
+    {
+        food, utilities, transportation, outing, groceries, clothes, electronics, other
+    }
+    public class CostItem
+    {
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public float Sum { get; set; }
+        public string Location { get; set; }
+        public DateTime Date { get; set; }
+        public string Currency { get; set; }
+        public CostType Type { get; set; }
+    }
